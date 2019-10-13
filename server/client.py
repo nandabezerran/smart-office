4596a31c955cfb7a0d5eca2b9b467824a3e7facc
ArinaSanches <sanchesarina96@gmail.com>
2019-10-13 17:49:13 -0300

    Correções no server e adição das funções da ddza

--- server/client.py
+++ server/client.py
@@ -1,18 +1,21 @@
 import socket
+import json
 host = socket.gethostbyname(socket.gethostname())
-server_ip = socket.gethostbyname(socket.gethostname())
 port = 5000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 addr = (host, int(port))
-msgF = "nANDINHA"
-client.bind((server_ip, port))
+x = {"tipo": "Ar-condicionado", "ip": "adosihdaiosddios", "id": "1", "porta": 5000,
+     "acoes": {"status": "Ligado",
+               "temperatura": 30}}
+
+client.bind((host, port))
 
 while True:
     msg = input("Servidor 1")
     rsp, server = client.recvfrom(1024)
     print(rsp)
     print(server)
-    client.sendto(msgF.encode(), server)
+    client.sendto(json.dumps(x).encode(), server)
 
--- server/server.py
+++ server/server.py
@@ -1,118 +1,138 @@
 #!flask/bin/python
 import socket
-# from flask import Flask
-# from flask import jsonify
-# import collections
+import json
+from flask import Flask, request, jsonify
+import collections
+import json
+from flask_cors import CORS
 
-# app = Flask(__name__)
-#
-# devices = []
-#
-# @app.route('/server/<string:id>/<string:status>', methods=['PUT'])
-# def changeStatus(id, status):
-#     resultList = []
-#     for device in devices:
-#         if(device.id == "id"):
-#             dp = collections.OrderedDict()
-#             dp = {'status': status}
-#             resultList.append(dp)
-#
-#             device.status = status
-#
-#             sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
-#             sock.sendto(jsonify(resultList), (device.ip, device.port))
-#
-# @app.route('/server/getDevices', methods=['GET'])
-# def getDevices(id, status):
-#     resultList = []
-#     for device in devices:
-#         # Mudar para protoBuffers
-#         dp = collections.OrderedDict()
-#         dp = {'id':device.id, 'status': device.status, 'acoes': jsonify(device.acoes)}
-#         resultList.append(dp)
-#     return jsonify(resultList)
-#
-# @app.route('/server/changeVolume/<string:volume>', methods=['PUT'])
-# def changeTvVolume(volume):
-#     resultList = []
-#     for device in devices:
-#         if(device.id == "tv"):
-#             dp = collections.OrderedDict()
-#             dp = {'volume': volume}
-#             resultList.append(dp)
-#
-#             device.acoes.volume = volume
-#
-#             sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
-#             sock.sendto(jsonify(resultList), (device.ip, device.port))
-#
-# @app.route('/server/changeChannel/<string:channel>', methods=['PUT'])
-# def changeTvChannel(channel):
-#     resultList = []
-#     for device in devices:
-#         if(device.id == "tv"):
-#             dp = collections.OrderedDict()
-#             dp = {'canal': channel}
-#             resultList.append(dp)
-#
-#             device.acoes.canal = channel
-#
-#             sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
-#             sock.sendto(jsonify(resultList), (device.ip, device.port))
-#
-# @app.route('/server/changeAirTemp/<string:temp>', methods=['PUT'])
-# def changeAirTemp(temp):
-#     resultList = []
-#     for device in devices:
-#         if(device.id == "ar-condicionado"):
-#             dp = collections.OrderedDict()
-#             dp = {'temperatura': temp}
-#             resultList.append(dp)
-#
-#             device.acoes.temperatura = temp
-#
-#             sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
-#             sock.sendto(jsonify(resultList), (device.ip, device.port))
 
-# The connection with the client will be made with the flask routes, not by the pure TCP socket.
-# The sockets here are only for the connections with the dispositives that are made with UDP
+
+app = Flask(__name__)
+CORS(app)
+
+devices = []
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    teste = jsonify({"teste":"cu"})
+    print(type(teste))
+    return teste
+
+
+@app.route('/getDevices', methods=['GET'])
+def get_devices():
+    resultado = []
+    for device in devices:
+        dev = device
+        # dev['acoes'] = json.dumps(dev['acoes'], separators=(',', ':'))
+        resultado.append(dev)
+    # return json.dumps(resultado, separators=(',', ':'))
+    return jsonify(resultado)
+
+
+@app.route('/changeStatus/<string:id>/<string:new_status>', methods=["PUT"])
+def change_status(id, new_status):
+    dev = None
+    # print(new_status)
+    for device in devices:
+        if(device['id'] == id):
+            # print(device["acoes"])
+            device["acoes"]["status"] = str(new_status)
+            dev = device
+            dev['acoes'] = json.dumps(dev['acoes'], separators=(',', ':'))
+    return json.dumps(dev, separators=(',', ':'))
+#   Atualizar status do device
+#   sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
+#   sock.sendto(jsonify(resultList), (device.ip, device.port))
+
+
+@app.route('/changeTemp/<string:id>/<string:new_temp>', methods=["PUT"])
+def change_temperatura(id, new_temp):
+    dev = None
+    # print(new_status)
+    for device in devices:
+        if(device['id'] == id):
+            # print(device["acoes"])
+            device["acoes"]["temperatura"] = str(new_temp)
+            dev = device
+            dev['acoes'] = json.dumps(dev['acoes'], separators=(',', ':'))
+    return json.dumps(dev, separators=(',', ':'))
+#   Atualizar status do device
+#   sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
+#   sock.sendto(jsonify(resultList), (device.ip, device.port))
+
+
+@app.route('/changeCanal/<string:id>/<string:new_canal>', methods=["PUT"])
+def change_canal(id, new_canal):
+    dev = None
+    # print(new_status)
+    for device in devices:
+        if(device['id'] == id):
+            # print(device["acoes"])
+            device["acoes"]["status"] = str(new_canal)
+            dev = device
+            dev['acoes'] = json.dumps(dev['acoes'], separators=(',', ':'))
+    return json.dumps(dev, separators=(',', ':'))
+#   Atualizar status do device
+#   sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
+#   sock.sendto(jsonify(resultList), (device.ip, device.port))
+
+
+@app.route('/changeVolume/<string:id>/<string:new_volume>', methods=["PUT"])
+def change_volume(id, new_volume):
+    dev = None
+    # print(new_status)
+    for device in devices:
+        if(device['id'] == id):
+            # print(device["acoes"])
+            device["acoes"]["volume"] = str(new_volume)
+            dev = device
+            dev['acoes'] = json.dumps(dev['acoes'], separators=(',', ':'))
+    return json.dumps(dev, separators=(',', ':'))
+#   Atualizar status do device
+#   sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
+#   sock.sendto(jsonify(resultList), (device.ip, device.port))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000', debug=True)
+
+
+def decode_json(msg):
+    device = json.loads(msg)
+    devices.append(device)
+
+
 server_ip = socket.gethostbyname(socket.gethostname())
 server_port = 3000
 server_addr = (server_ip, int(server_port))
 
-host_broadcast = '255.255.255.255'
-port_broadcast = 5000
+broadcast_ip = '255.255.255.255'
+broadcast_port = 5000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# sock.settimeout(5)
 
-broacast_addr = (host_broadcast, port_broadcast)
+broacast_addr = (broadcast_ip, broadcast_port)
 
 sock.bind(server_addr)
-print("server criado")
 
-msg = "ssssss"
+msg = "Solicitando conexao..."
 
 try:
 
     sock.sendto(msg.encode(), broacast_addr)
 
     while True:
-        print("etrou")
         data, client = sock.recvfrom(1024)
-        print(data)
-        print(client)
-        sock.sendto(msg.encode(), client)
+        decode_json(data.decode())
+        print(devices[0])
 
 finally:
 	sock.close()
 
-# while True:
-# 	print ("Listening")
-#
-#     msg, disp = udp.recvfrom(1024) # Gets the IP of the dispositive
-    # msg = msg.decode() # Gets info from the dispositive
-    # msg.ParseFromString(msg) # Parse the protobuf binary message
-	# dispositive.append(msg); # Add the dispositive to the vector
+
+
