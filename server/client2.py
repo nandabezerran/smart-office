55e8cc7d36e6a165ea4b77234af05cabf2654df3
ArinaSanches <sanchesarina96@gmail.com>
2019-10-15 23:20:06 -0300

    Mudança do broadcast, para permitit multiplos clientes

--- server/client2.py
+++ server/client2.py
@@ -2,14 +2,13 @@ import socket
 import json
 host = socket.gethostbyname(socket.gethostname())
 port_broadcast = 5000
-port_client = 1000
+port_client = 2000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 addr = (host, int(port_broadcast))
-x = {"tipo": "Ar-condicionado", "ip": host, "id": "1", "porta": port_client,
-     "acoes": {"status": "Ligado",
-               "temperatura": 30}}
+x = {"tipo": "lampada", "ip": host, "id": "2", "porta": port_client,
+     "acoes": {"status": "Ligado"}}
 
 client.bind((host, port_broadcast))
 
@@ -23,4 +22,3 @@ while True:
     client.sendto(json.dumps(x).encode(), server)
     rsp, server = client.recvfrom(1024)
     print(rsp)
-
